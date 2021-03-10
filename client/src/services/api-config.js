1d4979fa7b0681bc0eebbac7d49787c880ae33a0
vinod kumar <vinod513kumar@gmail.com>
2021-03-10 16:38:22 -0500

    rename screen folder

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const baseUrl = process.env.NODE_ENV === 'production' ? "deployed link here" : 'http://localhost:3000'
+const baseUrl = process.env.NODE_ENV === 'production' ? "https://flavors-of-india-api.herokuapp.com/" : 'http://localhost:3000'
 
 const api = axios.create({
   baseURL: baseUrl
