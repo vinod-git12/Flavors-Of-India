e00cbee58759d8c43ca0215b57a3fa5dc0ee6c1b
vinod kumar <vinod513kumar@gmail.com>
2021-03-10 12:17:28 -0500

    added github logo in footer component

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
           href="https://github.com/vinod-git12"
           target="blank"
         >
-          <i className="fab fa-github"><img src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" alt=""></img> </i> &nbsp; Vinod Kumar
+          <img src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" alt="Github" className="footer-img"></img> &nbsp; Vinod Kumar
         </a>
        
       </div>
